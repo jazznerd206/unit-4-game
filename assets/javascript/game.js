34a02f306570dd9cb941ace37be9fefa69142254
Andrew Miller <206alm@gmail.com>
2019-09-18 12:39:46 -0700

    palyers can now be selected correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,61 @@
 var hanSolo = {
+    name: "Han Solo",
     health: 100,
+    attackStart: 15,
     attack: 15,
 }
 var ig88 = {
+    name: "IG-88",
     health: 120,
+    attackStart: 12,
     attack: 12,
 }
 var bobaFett = {
+    name: "Boba Fett",
     health: 180,
+    attackStart: 8,
     attack: 8,
 }
 var jabbaTheHutt = {
+    name: "Jabba The Hutt",
     health: 200,
+    attackStart: 2,
     attack: 2,
 }
+
+var wins = 0;
+var losses = 0;
+var fighterArray = [hanSolo, ig88, bobaFett, jabbaTheHutt];
+var userChar = {};
+var defender = {};
+var attackerSlotFilled = false;
+var defSlotfilled = false;
+var resetGame = false;
+var gameOver = false;
+
+
+//Select user character
+function charSelect() {
+    $(".char-choice").on("click", function () {
+        $("#user-pick-one").append($(".char-choice"));
+        attackerSlotFilled === true;
+        $(this).attr("class", "first-choice");
+        console.log(this);
+        $(".char-choice").removeAttr("class", "char-choice").attr("class", "availDef");
+        $("#choice-other").append($(".availDef"));
+    $(".availDef").on("click", function () {
+        $(this).removeAttr("class", "availDef").attr("class", "second-choice");
+        $("#opponent").append($(".second-choice"));
+        defSlotfilled === true;
+        console.log(this);
+    })
+    });
+}
+
+
+$(document).ready(function(){  
+
+    charSelect();
+    
+});
 
